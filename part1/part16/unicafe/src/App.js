794bbfc46eac81790d78bd67a52a1c24ef93e5fc
Jarno Varis <jarno.varis@gmail.com>
2022-10-17 21:33:30 +0300

    continued unicafe, got numbers ok (part 1.7), also took .env out and appended it to environment variables

--- part1/part16/unicafe/src/App.js
+++ part1/part16/unicafe/src/App.js
@@ -12,20 +12,37 @@ const Button = (props) => (
   </button>
 )
 
+const Statetext = ({name, value}) => (
+    <dt>
+      {name} {value}
+    </dt>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const header = "give feedback"
+  const average = (good + neutral + bad) === 0 ? 0 : (good - bad) / (good + neutral + bad)
+  const positive = ((good + neutral + bad) === 0 ? 0 : (good) / (good + neutral + bad) ) * 100
 
   return (
     <div>
-      < Header header={header} />
+      < Header header={"give feedback"} />
       < Button text={"good"} handleClick={() => setGood(good + 1)} />
       < Button text={"neutral"} handleClick={() => setNeutral(neutral + 1)} />
       < Button text={"bad"} handleClick={() => setBad(bad + 1)}/>
+      < Header header={"give feedback"} />
+
+      <dl>
+      < Statetext name={"good\n"} value={good} />
+      < Statetext name={"neutral"} value={neutral} />
+      < Statetext name={"bad"} value={bad} />
+      < Statetext name={"all"} value={good + bad + neutral} />
+      < Statetext name={"average"} value={average} />
+      < Statetext name={"positive"} value={positive.toString() + " %"} />
+      </dl>
     </div>
   )
 }
