25e942826ee9a553bcd305641d29be515238b791
Jarno Varis <jarno.varis@gmail.com>
2022-10-18 18:55:01 +0300

    statistics to table, no errors!

--- part1/part16/unicafe/src/App.js
+++ part1/part16/unicafe/src/App.js
@@ -12,10 +12,35 @@ const Button = (props) => (
   </button>
 )
 
-const Statetext = ({name, value}) => (
-    <dt>
-      {name} {value}
-    </dt>
+const Buttons = (props) => {
+  
+  const good = props["hooks"]["good"][0]
+  const setGood = props["hooks"]["good"][1]
+  const neutral = props["hooks"]["neutral"][0]
+  const setNeutral = props["hooks"]["neutral"][1]
+  const bad = props["hooks"]["bad"][0]
+  const setBad = props["hooks"]["bad"][1]
+
+  return (
+    <>
+      < Button text={"good"} handleClick={() => setGood(good + 1)} />
+      < Button text={"neutral"} handleClick={() => setNeutral(neutral + 1)} />
+      < Button text={"bad"} handleClick={() => setBad(bad + 1)}/>
+  </>
+  )
+}
+
+// Statetext basically alreadyd did the same thing as StatisticsLine, but
+// I made this component too just to be sure, hopefully I didn't misunderstand
+const StatisticsLine = ({name, value}) => (
+  <tr>
+    <td>
+      {name}
+    </td>
+    <td>
+      {value}
+    </td>
+  </tr>
 )
 
 const Statistics = ({good, neutral, bad}) => {
@@ -32,21 +57,21 @@ const Statistics = ({good, neutral, bad}) => {
     const positive = (good) / (good + neutral + bad) * 100
   
     return (
-      <dl>
-        < Statetext name={"good\n"} value={good} />
-        < Statetext name={"neutral"} value={neutral} />
-        < Statetext name={"bad"} value={bad} />
-        < Statetext name={"all"} value={good + bad + neutral} />
-        < Statetext name={"average"} value={average} />
-        < Statetext name={"positive"} value={positive.toString() + " %"} />
-      </dl>
+      <table>
+        <tbody>
+          < StatisticsLine name={"good\n"} value={good} />
+          < StatisticsLine name={"neutral"} value={neutral} />
+          < StatisticsLine name={"bad"} value={bad} />
+          < StatisticsLine name={"all"} value={good + bad + neutral} />
+          < StatisticsLine name={"average"} value={average} />
+          < StatisticsLine name={"positive"} value={positive.toString() + " %"} />
+        </tbody>
+      </table>
     )
   }
 
   
 }
-
-
 
 const App = () => {
   // save clicks of each button to its own state
@@ -54,13 +79,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const hooks = {
+    good: [good, setGood],
+    neutral: [neutral, setNeutral],
+    bad: [bad, setBad]
+  }
+
   return (
     <div>
-      < Header header={"give feedback"} />
-      < Button text={"good"} handleClick={() => setGood(good + 1)} />
-      < Button text={"neutral"} handleClick={() => setNeutral(neutral + 1)} />
-      < Button text={"bad"} handleClick={() => setBad(bad + 1)}/>
-      < Header header={"statistics"} />
+        < Header header={"give feedback"} />
+        < Buttons hooks={hooks} />
+        < Header header={"statistics"} />
 
       < Statistics good={good} neutral={neutral} bad={bad} />
     </div>
