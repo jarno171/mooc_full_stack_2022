24f4d64c9d919ec33d60ecf6cb0e52c69b8d38ab
Jarno Varis <jarno.varis@gmail.com>
2022-10-18 18:16:51 +0300

    refactored statistics

--- part1/part16/unicafe/src/App.js
+++ part1/part16/unicafe/src/App.js
@@ -18,14 +18,30 @@ const Statetext = ({name, value}) => (
     </dt>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+
+  const average = (good + neutral + bad) === 0 ? 0 : (good - bad) / (good + neutral + bad)
+  const positive = ((good + neutral + bad) === 0 ? 0 : (good) / (good + neutral + bad) ) * 100
+
+  return (
+    <dl>
+      < Statetext name={"good\n"} value={good} />
+      < Statetext name={"neutral"} value={neutral} />
+      < Statetext name={"bad"} value={bad} />
+      < Statetext name={"all"} value={good + bad + neutral} />
+      < Statetext name={"average"} value={average} />
+      < Statetext name={"positive"} value={positive.toString() + " %"} />
+    </dl>
+  )
+}
+
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const average = (good + neutral + bad) === 0 ? 0 : (good - bad) / (good + neutral + bad)
-  const positive = ((good + neutral + bad) === 0 ? 0 : (good) / (good + neutral + bad) ) * 100
 
   return (
     <div>
@@ -35,14 +51,7 @@ const App = () => {
       < Button text={"bad"} handleClick={() => setBad(bad + 1)}/>
       < Header header={"give feedback"} />
 
-      <dl>
-      < Statetext name={"good\n"} value={good} />
-      < Statetext name={"neutral"} value={neutral} />
-      < Statetext name={"bad"} value={bad} />
-      < Statetext name={"all"} value={good + bad + neutral} />
-      < Statetext name={"average"} value={average} />
-      < Statetext name={"positive"} value={positive.toString() + " %"} />
-      </dl>
+      < Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
