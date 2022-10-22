933b42d5ae5deaa854c957925309b7ad43e0625e
Jarno Varis <jarno.varis@gmail.com>
2022-10-22 17:33:15 +0300

    did exercise 2.14, shows temp and picture too, had to use usePromise, dunno if there is better way? Could still refactor code by taking it into different components

--- part2/part212/countries/src/App.js
+++ part2/part212/countries/src/App.js
@@ -1,6 +1,32 @@
 import { useEffect, useState } from 'react'
+import usePromise from 'react-use-promise';
 
 import axios from 'axios'
+
+const countryInfo = (countryToFind, countryData) => {
+  // only take first = only row
+  const countryDataForInfo = countryData.filter((country) => {
+    return country.name.common === countryToFind
+  })[0]
+
+  return countryDataForInfo
+}
+
+const pullWeatherInfo = async (latitude, longtitude) => {
+
+  const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY
+
+  const urlRoot = "https://api.openweathermap.org/data/2.5/weather?"
+
+  const searchParams = new URLSearchParams()
+  searchParams.append("lat", latitude)
+  searchParams.append("lon", longtitude)
+  searchParams.append("appid", apiKey)
+
+  const url = urlRoot + searchParams.toString()
+
+  return axios.get(url)
+}
 
 const Button = (props) => {
   return (
@@ -11,15 +37,42 @@ const Button = (props) => {
     </>
   )}
 
+const pullCityWeather = async (countryForInfo, countryData) => {
+
+  if (countryForInfo !== "") {
+
+    // only take first = only row
+    const countryDataForInfo = countryInfo(countryForInfo, countryData)
+
+     // get weather data for location
+    const locationLat = countryDataForInfo.latlng[0]
+    const locationLong = countryDataForInfo.latlng[1]
+
+    return pullWeatherInfo(locationLat, locationLong)
+      .then(response => {
+        const tempKelvin = response.data.main.temp
+        const windSpeed = response.data.wind.speed
+        const icon = response.data.weather[0].icon
+        const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+        return {
+          temp: tempKelvin - 273.15,
+          wind: windSpeed,
+          url: iconUrl
+        }
+      })
+  } else {
+    return { }
+  }
+}
+
 const CountryInfo = ({countryForInfo, countryData}) => {
 
   /* Only draw if additional info country is set to be drawn */
   if (countryForInfo !== "") {
 
     // only take first = only row
-    const countryDataForInfo = countryData.filter((country) => {
-      return country.name.common === countryForInfo
-    })[0]
+    const countryDataForInfo = countryInfo(countryForInfo, countryData)
 
     return (
       <div>
@@ -41,15 +94,25 @@ const CountryInfo = ({countryForInfo, countryData}) => {
   }
 }
 
+const Weather = ({weather, countryToShowInfo}) => {
+  if ((weather) && (countryToShowInfo !== "")) {
+    return (
+      <>
+        < img src={weather.url} alt={"Weather"} />
+        <p>temperature: {weather.temp} celsius</p>
+        <p>wind speed: {weather.wind} m/s</p>
+      </>
+    )
+  }
+}
+
 const Filter = (props) => {
 
   const countriesFiltered = props.countries.filter((country) => {
     return country.includes(props.searchCriteria)
   })
 
-
   if (countriesFiltered.length <= 10) {
-
     return (
       <>
         {countriesFiltered.map(country => {
@@ -75,6 +138,8 @@ const App = () => {
   const [countryData, setCountryData] = useState({})
   const [searchCriteria, setSearchCriteria] = useState('')
   const [countryToShowInfo, setCountryToShowInfo] = useState('')
+
+  const [weather, error] = usePromise(() => pullCityWeather(countryToShowInfo, countryData), [countryToShowInfo])
 
   const handleFilterChange = (event) => {
     setSearchCriteria(event.target.value)
@@ -110,6 +175,8 @@ const App = () => {
       < Filter searchCriteria={searchCriteria} countries={countries} countryData={countryData} handleCountryToShowInfoChange={handleCountryToShowInfoChange} />
 
       < CountryInfo countryForInfo={countryToShowInfo} countryData={countryData} />
+      < Weather weather={weather} countryToShowInfo={countryToShowInfo} />
+      {error}
     </div>
   )
 }
